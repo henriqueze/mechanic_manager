d8ba86e56cf752a2dacd04b2a4b158c8dfc0bac6
Henrique Poncetti Zegalo <henrique.ze@live.com>
2019-05-09 15:33:36 -0300

    adjusting cities

--- db/migrate/20190509175419_create_cities.rb
+++ db/migrate/20190509175419_create_cities.rb
@@ -2,7 +2,7 @@ class CreateCities < ActiveRecord::Migration[5.2]
   def change
     create_table :cities do |t|
       t.string :name
-      t.references :State, foreign_key: true
+      t.references :states, foreign_key: true
 
       t.timestamps
     end
