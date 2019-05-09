b06fd60f4b479173a52c6537f873ac7dea3978ae
Henrique Poncetti Zegalo <henrique.ze@live.com>
2019-05-09 15:10:10 -0300

    scaffold people

---
+++ app/views/people/_person.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! person, :id, :kind, :name, :company_name, :ie, :cnpj, :rg, :cpf, :tel1, :tel2, :email, :address, :address_number, :address_complement, :address_bairro, :address_zip, :status, :created_at, :updated_at
+json.url person_url(person, format: :json)
---
+++ db/migrate/20190509180949_create_people.rb
@@ -0,0 +1,24 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :kind
+      t.string :name
+      t.string :company_name
+      t.bigint :ie
+      t.bigint :cnpj
+      t.bigint :rg
+      t.bigint :cpf
+      t.bigint :tel1
+      t.bigint :tel2
+      t.string :email
+      t.string :address
+      t.string :address_number
+      t.string :address_complement
+      t.string :address_bairro
+      t.bigint :address_zip
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
