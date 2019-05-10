a2d81c2024a86347ca2724b7f4c665e6f7704f7e
Henrique Poncetti Zegalo <henrique.ze@live.com>
2019-05-10 08:31:29 -0300

    disable scaffold stylesheets

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,9 @@ module MechanicManager
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
     # Don't generate system test files.
     config.generators.system_tests = nil
+    #no css scaffold
+    config.generators.scaffold_stylesheet false
   end
 end
